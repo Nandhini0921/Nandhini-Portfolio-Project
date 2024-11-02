8340cdd443027dc4d8d551c44f01c54c21c85c01
Nandhini0921 <nandhini.0921@gmail.com>
2024-11-02 06:30:43 -1200

    first commit

---
+++ Portfolio.js
@@ -0,0 +1,9 @@
+// Add event listener to contact form
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    console.log(Name: ${name}, Email: ${email}, Message: ${message});
+});
